5be981a4a43cfbd66f437f389c64b1e1ed256372
Ivan Reyes <ivinreyes1@gmail.com>
2021-02-28 23:28:52 -0600

    exercise 2

---
+++ guia7_ejercicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿ using System;
+
+namespace guia7_ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "GUIA 7 EJERCICIO 2 ";
+            int x = 0;
+
+            Console.WriteLine("escribe un numero entre el 1 y el 15");
+            x = int.Parse(Console.ReadLine());
+            if (x <= 15)
+            {
+                for (int y = 1; y <= x; y++)
+                {
+                    Console.WriteLine("*");
+                }
+            }
+            else
+            Console.ReadKey();
+        }
+    }
+}
